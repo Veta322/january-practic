e163a701d88d8847a35bfca4e32d7d825c426a09
Veta322 <vetarejh@gmail.com>
2023-01-23 21:39:36 +0300

    2 dz

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
-let money = prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
+let money, time; 
 
-const appData = {
-    budget: money,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: time,
-	savings: false
+function start (){
+    money = +prompt ("Ваш бюджет на месяц?", "");
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+    
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt ("Ваш бюджет на месяц?", "");
+    }
+}
+
+start();
+
+
+let appData = {
+budget: money,
+timeData: time,
+expenses: {},
+optionalExpenses: {},
+income: [],
+savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
+function chooseExpenses(){ 
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+for (let i = 0; i < 2; i++) {
+let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+    b = prompt ("Во сколько обойдется?", "");
 
-alert(appData.budget / 30);
+if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+    console.log ("done");
+
+    appData.expenses[a] = b;
+} else {                            
+    console.log ("bad result");
+    i--;
+}
+
+}
+
+};
+
+chooseExpenses();
+
+
+
+appData.moneyPerDay = (appData.budget / 30).toFixed;
+
+
+alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+
+
+if (appData.moneyPerDay < 100) {
+console.log ("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+console.log ("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+console.log ("Это высокий уровень достатка!");
+} else {
+console.log ("Произошла ошибка");
+}
